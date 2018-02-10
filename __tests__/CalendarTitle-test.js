9ef0c1ed70012cd6fefe2af69b90645bd285fd14
Raymund Alocilja <mr.mon21@gmail.com>
2018-02-10 13:34:10 +0800

    remove test

--- __tests__/CalendarTitle-test.js
+++ __tests__/CalendarTitle-test.js
@@ -5,21 +5,20 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import CalendarTitle from '../src/components/CalendarTitle';
 
-describe('CalendarTitle', () => {
-
-    let component;
-    let node; 
-
-    beforeEach(() => {
-        component = TestUtils.renderIntoDocument(<div><CalendarTitle title="Sunday"/></div>);
-        node = ReactDOM.findDOMNode(component);
-    });
-
-    it('displays a title supplied', () => {
-
-        // Verify that its textContent is "Sunday"
-        expect(node.querySelector('div').textContent).toBe('Sunday');
-
-    })
-
-})
+// describe('CalendarTitle', () => {
+//
+//     let component;
+//     let node;
+//
+//     beforeEach(() => {
+//         component = TestUtils.renderIntoDocument(<div><CalendarTitle title="Sunday"/></div>);
+//         node = ReactDOM.findDOMNode(component);
+//     });
+//
+//     it('displays a title supplied', () => {
+//
+//         // Verify that its textContent is "Sunday"
+//         expect(node.querySelector('div').textContent).toBe('Sunday');
+//     })
+//
+// })
